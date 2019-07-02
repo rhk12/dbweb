10317c5cbf9fb3456c30644fdb39778c04f0fcd6
Dipak Kumar <dipakkumar@Dipak.Kumar>
2019-07-02 22:22:42 +0530

    design change on home page

--- client/src/pages/Home/FirstComponent.js
+++ client/src/pages/Home/FirstComponent.js
@@ -5,22 +5,10 @@ export default () => {
         <div className="component first-component">
            <section>
                 <div className="bannerArea">
-                <MDBRow className="bsub">
-                    <MDBCol size="7">
-                    <img className="banrimage" src="/img/banner/Group 838.png" alt="" />
-                    </MDBCol>
-                    <MDBCol size="5" className="bannerText">
-                    <div className="subtext">
-                        <div className="hd">Digital Brain Technologies </div>
-                        <div className="sub">
-                        <p>
-                            We provide a sensor-enabled cloud based platform for monitoring
-                            brain health.{" "}
-                        </p>
-                        </div>
-                    </div>
-                    </MDBCol>
-                </MDBRow>
+                <div className="bannerText">
+                <h4>MONITOR YOUR BRAIN HEALTH</h4>
+                <p>Accurate brain simulations to help transform your sensor data into actionable insights about brain trauma</p>
+                </div>
                 </div>
             </section>
         </div>
